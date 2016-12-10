fc94efa810e48fecb9c569f65f4932c01ffaf759
mcalbertkay <mcalbertkay@gmail.com>
2016-12-10 14:34:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+$botToken = "";//inserire il token del proprio bot
+$website = "https://api.telegram.org/bot".$botToken;
 $update = file_get_contents("php://input");//La pagina prende le il codice della pagina chiamate (bot api)
 $updateArray = json_decode($update, TRUE);//trasfroma le informazione in un formato json da leggere 
 $chatId = $updateArray["message"]["chat"]["id"];//prende l'id della chat
