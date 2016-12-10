53cf1c70af08ce81bca8e16e8b96925631380a1b
mcalbertkay <mcalbertkay@gmail.com>
2016-12-10 14:17:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
-
 $update = file_get_contents("php://input");//La pagina prende le il codice della pagina chiamate (bot api)
 $updateArray = json_decode($update, TRUE);//trasfroma le informazione in un formato json da leggere 
 $chatId = $updateArray["message"]["chat"]["id"];//prende l'id della chat
-file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=Funzionaaa!!Huahuahau");//spedisce il messaggio tramite get method
-
-
-
-
+file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test");//spedisce il messaggio tramite get method
 ?>
